cdfacc784366659a34b319e0bc0d0342fadde19e
chong <44306843+GabrielRalph@users.noreply.github.com>
2020-12-29 17:46:21 +1100

    Create Sammy.js

---
+++ Sammy.js
@@ -0,0 +1,552 @@
+class Sammy{
+  references = [
+    {
+      name: "Stephen Potter",
+      location: "Freda",
+      role: "Head of AV Tech",
+      email: "s.potter.promo@gmail.com",
+      number: "0421 761 305"
+    },
+    {
+      name: "Michael Mclennan",
+      location: "Sydney Film School",
+      role: "Head of Film School",
+      email: "mclennan.michael@gmail.com",
+      number: "0432 085 370"
+    }
+  ]
+
+  contacts = {
+    address: "St Peters, 2044<br />Sydney NSW",
+    number: "0404 858 205",
+    email: "sammy.pinto.sfs@gmail.com",
+    instagram: `<a href="https://www.instagram.com/ripsammypinto2016/">@ripsammypinto2016</a>
+                     <a href="https://www.instagram.com/minipinto/">@minipinto</a>`
+  }
+
+  filmRoles = [
+    "Director of Photography",
+    "Camera Operator",
+    "Camera Assistant",
+    "Sound Recordist",
+    "Sound Designer",
+    "Sound Mixer",
+    "Grip",
+    "Gaffer",
+    "Boom Swinger",
+    "Runner",
+    "Best Boy",
+  ]
+
+  awards = [
+    "Best Cinematographer: Diploma of Screen and Media Part 2 - Sammy Pinto ‘Jesus Piece’",
+    "Most Provocative Film - ‘WTF TV PRESENTS: SASHA’",
+    "Duke of Edinburgh Scheme- Bronze, Silver and Gold",
+    "Employee of the month-Hoyts",
+    "The Scarf Foundation Award for Commitments to the School Environment",
+    "Received many awards/trophies in sporting achievements",
+  ]
+
+  qualifications = [
+    "RSA",
+    "RCG",
+    "Full Drivers Licence",
+    "2017 - Sydney Film School",
+  ]
+  experiences = [
+    {
+      title: "Freelance",
+      location: "Sydney",
+      role: "Photography, Film, Directing, Producing, Camera Assisting and Sound",
+      date: "January 2014 - Present",
+    },
+    {
+      title: "Frankie's Pizza",
+      location: "Sydney",
+      role: "Barback",
+      date: "April 2019 - Present",
+    },
+    {
+      title: "Fredas",
+      location: "Chippendale",
+      role: "AV tech and Barback",
+      date: "April 2019 - November 2020",
+    },
+    {
+      title: "The George",
+      location: "Waterloo",
+      role: "Bartender, Gaming Attendant and Food runner",
+      date: "March 2019 - May 2019",
+    },
+    {
+      title: "Hoyts",
+      location: "Broadway",
+      role: "Usher, Boxsale and CandyBar",
+      date: "January 2018 - April 2019",
+    },
+    {
+      title: "Black Comedy",
+      location: "ABC",
+      role: "Camera Assistant",
+      date: "2018",
+    },
+    {
+      title: "Sydney Underground Film Festival",
+      location: " Marrickville",
+      role: "Usher and Merch",
+      date: "September 14th - 20th 2017",
+    },
+    {
+      title: "Tropfest",
+      location: "Parramatta",
+      role: "Volunteer",
+      date: "2016",
+    },
+    {
+      title: "Anthony Khoury Photography",
+      location: "Lane Cove",
+      role: "Work Experience",
+      date: "July 14th - 18th 2014",
+    },
+    {
+      title: "Tara Anglican School For Girls",
+      location: "North Parramatta",
+      role: "School Photographer",
+      date: "2012 - 2016",
+    },
+  ]
+
+  education = [
+{
+      title: "Tara Anglican School For Girls",
+      location: "North Parramatta",
+      qualifications: "Kindergarten - Year 12 HSC",
+      date: "March 2004 - December 2016",
+},
+{
+      title: "Sydney Film School",
+      location: "Green Square",
+      qualifications: "Diploma of Screen and Media, Advanced Certificate in Film Sound ",
+      date: "February 2017 - December 2018",
+}];
+
+
+  projects = [
+    {
+      title: "Out of my mind",
+      date: "2017",
+      role: "Camera Operator",
+      description: "Drama 16mm short film, Sydney Film School",
+      video: "http link"
+    },
+    {
+      title: "Panopticon",
+      date: "2017",
+      role: "Best Boy/Runner",
+      description: "Sci-fi 16mm short film, Sydney Film School",
+      video: "http link"
+    },
+    {
+      title: "Veteran",
+      date: "2017",
+      role: "Grip/Gaffer/Runner",
+      description: "Drama 16mm short film, Sydney Film School",
+      video: "http link"
+    },
+    {
+      title: "The Dilemma of Choice and Choosing Nothing Possible",
+      date: "2017",
+      role: "2nd AC",
+      description: "Comedy short film, Sydney Film School",
+      video: "http link"
+    },
+    {
+      title: "Dogma, Reformation",
+      date: "2020",
+      role: "Camera Operator",
+      description: "Music Video",
+      video: "https://youtu.be/0_APKCN6c8E "
+    },
+    {
+      title: "The Flower (post production) ",
+      date: "2020",
+      role: "Camera Operator",
+      description: "Short film",
+
+    },
+    {
+      title: "Rosa Racer, Sober [music video](post production)",
+      date: "2020",
+      role: "Director",
+      description: "Music Video",
+    },
+    {
+      title: "Hayley Mary, Live show 2020 (post production)",
+      date: "2020",
+      role: "Camera Operator",
+      description: "Live show recording at Paddo RSL",
+    },
+    {
+      title: "The Melodrones, I’ll belong to you [Live performance]",
+      date: "2020",
+      role: "Camera Operator",
+      description: "Live set recording at Paddo RSL",
+      video: "https://www.instagram.com/tv/CIfFCmsBLBs/?hl=en"
+    },
+    {
+      title: "Hayley Mary, The Chain [Nude version]",
+      date: "2020",
+      role: "Camera Operator ",
+      description: "Music Video",
+      video: "https://youtu.be/oXBfKFxOq1o "
+    },
+    {
+      title: "DMA’S, Round & Around [Official Video] ",
+      date: "2020",
+      role: "Production Assistant and Camera Operator",
+      description: "Music Video",
+      video: "https://youtu.be/Qh-pJHglDdk"
+    },
+    {
+      title: "Only if you're lucky (post production)",
+      date: "2019",
+      role: "Director and Actor",
+      description: "Short film ",
+    },
+    {
+      title: "Havas, Everyday",
+      date: "2019",
+      role: "Camera Operator",
+      description: "Season II Episode 5 - Carpark",
+      video: "https://youtu.be/tswYTMagdW4"
+    },
+    {
+      title: "Nostalgia",
+      date: "2019",
+      role: "2nd AC",
+      description: "Short film",
+    },
+    {
+      title: "Pendulum",
+      date: "2019",
+      role: "2nd AC/2nd Camera Operator/Design Assistant",
+      description: "Short film",
+    },
+    {
+      title: "Bare Intimacy",
+      date: "2019",
+      role: "2nd AC/Gaffer",
+      description: "Short film",
+    },
+    {
+      title: "Projection",
+      date: "2019",
+      role: "2nd AC/Unit B Camera Operator/Sound Recordist",
+      description: "Short film",
+    },
+    {
+      title: "Detached",
+      date: "2019",
+      role: "Sound Recordist/Sound designer",
+      description: "Short film",
+    },
+    {
+      title: "Rosemary’s Way",
+      date: "2019",
+      role: "Camera Assistant",
+      description: "Documentary Feature film",
+    },
+    {
+      title: "Pananampalataya",
+      date: "2018",
+      role: "2nd AC",
+      description: "Short film",
+    },
+    {
+      title: "Backstage Backstab",
+      date: "2018",
+      role: "DOP",
+      description: "Short film",
+    },
+    {
+      title: "Claire",
+      date: "2018",
+      role: "DOP",
+      description: "Short film",
+    },
+    {
+      title: "Garbvati Sannata",
+      date: "2018",
+      role: "2nd AC",
+      description: "Short film",
+    },
+    {
+      title: "The Last Night of Summer",
+      date: "2018",
+      role: "Gaffer",
+      description: "Short film",
+    },
+    {
+      title: "Bystander",
+      date: "2018",
+      role: "2nd AC",
+      description: "Short film",
+    },
+    {
+      title: "Prehension",
+      date: "2018",
+      role: "Sound recordist/Sound designer/Sound mixer",
+      description: "Short film",
+    },
+    {
+      title: "Mawar",
+      date: "2018",
+      role: "2nd AC",
+      description: "Short film",
+    },
+    {
+      title: "Barnett",
+      date: "2018",
+      role: "2nd AC",
+      description: "Short film",
+    },
+    {
+      title: "Cosmic Dancer",
+      date: "2018",
+      role: "2nd AC",
+      description: "Short film",
+    },
+    {
+      title: "Greenery Interlude",
+      date: "2018",
+      role: "Sound recordist/Sound designer/Sound mixer",
+      description: "Short film",
+    },
+    {
+      title: "AAI: Abnormal Australian Investigations",
+      date: "2018",
+      role: "2nd AC",
+      description: "Short film",
+    },
+    {
+      title: "The Harbinger of Apollo",
+      date: "2018",
+      role: "Best boy",
+      description: "Short film",
+    },
+    {
+      title: "Against All Odds",
+      date: "2018",
+      role: "Sound recordist/Sound designer/Sound mixer",
+      description: "Short film",
+    },
+    {
+      title: "Bad Math, Oceans",
+      date: "2018",
+      role: "Gaffer",
+      description: "Music Video",
+      video: "https://vimeo.com/299999250 "
+    },
+    {
+      title: "Toyboy",
+      date: "2018",
+      role: "Gaffer/2nd AC",
+      description: "Feature film",
+    },
+    {
+      title: "Rackett, Alive",
+      date: "2018",
+      role: "Gaffer",
+      description: "Music video",
+      video: "https://youtu.be/8KYJ0mPIbxM"
+    },
+    {
+      title: "Ballet De La Nuit",
+      date: "2018",
+      role: "Stills Photographer/Best Boy/Runner",
+      description: "Feature film",
+    },
+    {
+      title: "Harper's Bazaar India, Vivid",
+      date: "2018",
+      role: "BTS",
+      description: "Fashion shoot",
+    },
+    {
+      title: "Action stars audition",
+      date: "2017",
+      role: "DOP",
+      description: "Tv show",
+    },
+    {
+      title: "Fractured",
+      date: "2017",
+      role: "2nd AC",
+      description: "Short film",
+    },
+    {
+      title: "The List",
+      date: "2017",
+      role: "Sound Recordist",
+      description: "2017",
+    },
+    {
+      title: "Missing girl",
+      date: "2017",
+      role: "Runner",
+      description: "Funding for feature film",
+    },
+    {
+      title: "In transit",
+      date: "2017",
+      role: "Producer/2nd AC/Design Assistant/Sound Recordist",
+      description: "Short film",
+    },
+    {
+      title: "Jesus Piece",
+      date: "2017",
+      role: "DOP",
+      description: "Short film",
+      video: "https://youtu.be/BtXFMV1Hi-w"
+    },
+    {
+      title: "How to Disappear",
+      date: "2017",
+      role: "2nd AC/Sound Recordist/Sound Designer/Runner",
+      description: "Short film",
+    },
+    {
+      title: "By Design",
+      date: "2017",
+      role: "Sound recordist/2nd AC/Runner",
+      description: "Short film",
+    },
+    {
+      title: "Toast time",
+      date: "2017",
+      role: "DOP",
+      description: "Short film",
+    },
+    {
+      title: "Broken Minds",
+      date: "2017",
+      role: "Gaffer/2nd AC/Runner",
+      description: "Short film",
+    },
+    {
+      title: "Circular",
+      date: "2017",
+      role: "2nd AC/Gaffer/Runner",
+      description: "Short film",
+    },
+    {
+      title: "Stand Off",
+      date: "2017",
+      role: "2nd AC/Gaffer",
+      description: "Short film",
+    },
+    {
+      title: "Screened",
+      date: "2017",
+      role: "Sound Recordist/Gaffer",
+      description: "Short film",
+    },
+    {
+      title: "Hang in Hell",
+      date: "2017",
+      role: "DOP",
+      description: "Short film",
+    },
+    {
+      title: "I’m Sorry for your Loss",
+      date: "2017",
+      role: "2nd AC/Gaffer/Runner/Best Boy",
+      description: "Short film",
+    },
+    {
+      title: "The Bluebird",
+      date: "2017",
+      role: "1st AC/2nd AC/Production Assistant",
+      description: "Short film",
+    },
+    {
+      title: "Crimsons Suits",
+      date: "2017",
+      role: "2nd AC/ Sound Recordist/2nd Camera Operator",
+      description: "Short film",
+    },
+    {
+      title: "WTF TV PRESENTS: Sasha",
+      date: "2017",
+      role: "Director/Writer/Producer/Editor/Sound Recordist/Camera Operator/Actor",
+      description: "Short film",
+    },
+    {
+      title: "Lily",
+      date: "2017",
+      role: "Gaffer/1st AC",
+      description: "Short film",
+    },
+    {
+      title: "Sydney Film School AD",
+      date: "2017",
+      role: "2nd AC",
+      description: "Commercial AD",
+    },
+    {
+      title: "Memoir",
+      date: "2017",
+      role: "Gaffer",
+      description: "Short film",
+    },
+    {
+      title: "The Oracle",
+      date: "2017",
+      role: "1st AC/Best Boy/Stills Photographer",
+      description: "Documentary short film",
+    },
+    {
+      title: "Heart to Heart",
+      date: "2017",
+      role: "DOP",
+      description: "Documentary short film",
+    },
+    {
+      title: "The Wooden Sword",
+      date: "2017",
+      role: "Camera Operator",
+      description: "Documentary short film",
+    },
+    {
+      title: "Life's Symphony",
+      date: "2017",
+      role: "Camera operator",
+      description: "Documentary short film",
+    },
+    {
+      title: "Affixed",
+      date: "2017",
+      role: "3rd AC",
+      description: "Short film",
+    },
+    {
+      title: "Colour",
+      date: "2017",
+      role: "2nd AC",
+      description: "Short film",
+    },
+    {
+      title: "Strings",
+      date: "2017",
+      role: "2nd AC/ Grip",
+      description: "Short film",
+      video: "http link"
+    },
+    {
+      title: "Out of my mind",
+      date: "2017",
+      role: "Camera Operator",
+      description: "Short Film",
+    }
+  ]
+}
