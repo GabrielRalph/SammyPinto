c4a6d184f8706a2c4b54b4d93342ee7819a0f61c
GabrielRalph <gltralph@gmail.com>
2021-05-06 10:31:29 +1000

    sober preview

--- Resume.js
+++ Resume.js
@@ -6,11 +6,58 @@ import {VList} from './VList.js'
 class Resume extends SvgPlus{
   constructor(data){
     super('DIV');
-    let row = this.createChild("TABLE").createChild("TBODY").createChild("TR");
+    this.class = "resume"
+    this.makeVideo();
+    this.bg = 0;
+
+    this.table = this.createChild("TABLE")
+    this.table.onwheel = (e) => {
+      this.fader(e)
+    }
+    let row = this.table.createChild("TBODY").createChild("TR");
     this.columnA = row.createChild("TD");
     this.columnB = row.createChild("TD");
 
     this.data = data;
+    window.scrollTo(0,0)
+  }
+
+  makeVideo(){
+    let video = this.createChild("video");
+    video.autoplay = true;
+    video.muted = true;
+    video.loop = true;
+    video.props = {
+      style: {
+        position: 'fixed',
+        top: "-18%",
+        left: "50%",
+        right: "0",
+        bottom: "0",
+        'z-index': '-1',
+        'height': `max(135vh, ${135*665/1183}vw)`,
+        transform: 'translate(-50%, 0)'
+      }
+    }
+    // video.oncanplay = () => {
+    //   document.body.onmousemove = ()=>{
+    //     console.log('x');
+    //     video.play()
+    //     document.body.onmousemove = null;
+    //   }
+    // }
+    video.createChild("source").props = {
+      src:"./Sober.mp4",
+      type: "video/mp4"
+    }
+  }
+
+  fader(e){
+    let box = this.table.getBoundingClientRect();
+    this.bg = (50-box.y)/window.innerHeight;
+    this.styles = {
+      background: `rgba(255,255,255, ${this.bg > 1 ? 1 : this.bg})`,
+    }
   }
 
   clear(){
@@ -37,6 +84,8 @@ class Resume extends SvgPlus{
     this.columnB.appendChild(new VList("Awards", data.awards, ListH4))
     this.columnB.appendChild(new VList("Qualifications", data.qualifications, ListH4))
   }
+
+  onscrl
 }
 
 export {Resume}
